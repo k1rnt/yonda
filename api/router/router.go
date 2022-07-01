d835810dc512b06c8866dc83a2119a17e467dee5
k1rnt <eru08tera15mora@gmail.com>
2022-07-02 03:43:53 +0900

    book_progressをbooksに紐ずくようにした

--- api/router/router.go
+++ api/router/router.go
@@ -19,6 +19,7 @@ func Init() {
 	{
 		v1.GET("/", actions.BookAllAction{Conn: conn}.Invoke)
 		v1.GET("/book/:id", actions.BookDetailAction{Conn: conn}.Invoke)
+		v1.POST("/book/:id", actions.BookReadAction{Conn: conn}.Invoke)
 		v1.POST("/book/register", actions.BookRegisterAction{Conn: conn}.Invoke)
 	}
 	e.GET("/ping", actions.PingAction{}.Invoke)
--- database/migrate/20220621171414_create_books_progress_table.down.sql
+++ database/migrate/20220621171414_create_books_progress_table.down.sql
@@ -1,1 +1,1 @@
-DROP TABLE IF EXISTS books_progress;
+DROP TABLE IF EXISTS books_progresses;
