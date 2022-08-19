a618ff5ed46f83c228a199609c93fdd02073e2c0
k1rnt <eru08tera15mora@gmail.com>
2022-08-20 00:45:06 +0900

    error responderの追加

--- api/internal/interface/actions/book_read.go
+++ api/internal/interface/actions/book_read.go
@@ -3,6 +3,7 @@ package actions
 import (
 	"github.com/jinzhu/gorm"
 	request "github.com/k1rnt/yonda/api/internal/interface/request/book"
+	eresponder "github.com/k1rnt/yonda/api/internal/interface/responder"
 	responder "github.com/k1rnt/yonda/api/internal/interface/responder/book"
 	usecase "github.com/k1rnt/yonda/api/internal/usecase/book"
 	"github.com/labstack/echo/v4"
@@ -17,20 +18,24 @@ type BookReadAction struct {
 func (action BookReadAction) Invoke(c echo.Context) error {
 	req := request.NewBookReadRequest()
 	if err := req.Param(c, "id"); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		resp := eresponder.NewErrorResponder(http.StatusBadRequest, err.Error())
+		return resp.Emit(c)
 	}
 	id, _ := strconv.ParseUint(req.ID, 10, 64)
 	u := usecase.NewBookReadUsecase(action.Conn)
 	ok := u.BookExist(id)
 	if !ok {
-		return echo.NewHTTPError(http.StatusNotFound, "Book not found")
+		resp := eresponder.NewErrorResponder(http.StatusNotFound, "Book not found")
+		return resp.Emit(c)
 	}
 	book := u.GetBook(id)
 	if err := req.Bind(c, book); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		resp := eresponder.NewErrorResponder(http.StatusBadRequest, "page must be integer")
+		return resp.Emit(c)
 	}
 	if err := u.Read(req); err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		resp := eresponder.NewErrorResponder(http.StatusInternalServerError, err.Error())
+		return resp.Emit(c)
 	}
 
 	resp := responder.NewReadBookResponder(http.StatusOK, "Book read success")
